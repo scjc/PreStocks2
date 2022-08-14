13a66ae984dbd1c0afafd578df8f3ef7f1c06bf9
Jeremy <30488840+scjc@users.noreply.github.com>
2022-08-14 02:36:13 +0100

    init tab view
    Added 3 views: Today, Trends and News with placeholder views

---
+++ PreStocks2/PreStocks2/TrendsView.swift
@@ -0,0 +1,20 @@
+//
+//  TrendsView.swift
+//  PreStocks2
+//
+//  Created by Jeremy Chang on 14/08/2022.
+//
+
+import SwiftUI
+
+struct TrendsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TrendsView_Previews: PreviewProvider {
+    static var previews: some View {
+        TrendsView()
+    }
+}
