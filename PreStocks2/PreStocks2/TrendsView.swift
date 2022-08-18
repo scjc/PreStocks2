95cbcc8e42229f3129bdd633e9b597ec2507d583
Jeremy <30488840+scjc@users.noreply.github.com>
2022-08-19 01:39:59 +0800

    Added list and search bar to trends view

--- PreStocks2/PreStocks2/TrendsView.swift
+++ PreStocks2/PreStocks2/TrendsView.swift
@@ -8,9 +8,38 @@
 import SwiftUI
 
 struct TrendsView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
+        animation: .default)
+    
+    private var items: FetchedResults<Item>
+    
+    @State private var searchText = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List {
+                ForEach(items) { item in
+                    NavigationLink {
+                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+                    } label: {
+                        Text(item.timestamp!, formatter: itemFormatter)
+                    }
+                }
+            }
+        }
+        .searchable(text: $searchText, prompt: "Search for a company...")
+        .navigationTitle("Trends")
     }
+
+    private let itemFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .medium
+        return formatter
+    }()
 }
 
 struct TrendsView_Previews: PreviewProvider {
